7583034672ab571c1d26726001c09a2f8cf20b90
st sama <albertezoula98@gmail.com>
2024-07-13 14:44:12 +0000

    adding the possibility for a user to add, update, delete, read the school he created

--- backend/routes/schools.js
+++ backend/routes/schools.js
@@ -43,57 +43,14 @@ router.post("/", async (req, res) => {
 
         await new School(newSchool).save();
         res.status(200).json({ message: 'School created successfully!' });
-        
-
-        // try {
-        //     let { name, address, location, phoneNumber, description, successRate, reviews, likes, dislikes, trainings} = req.body;
-        //     if (!name || !address || !location || !phoneNumber || !description) {
-        //         return res.status(400).json({ message: 'Fill the required fields' });
-        //     }
-        //     let thumbnail = req.files[0];
-        //     const userId = req.session.user._id;
-        //     if (thumbnail.size > 2000000) {
-        //         return res.status(400).json({ message: 'Image size should not exceed 2MB' });
-        //     }
-        //     let fileName = thumbnail.name;
-        //     let splitedName = fileName.split('.');
-        //     let newFileName = splitedName[0] + UUID() + '.' + splitedName[splitedName.length - 1];
-        //     thumbnail.mv(path.join(__dirname, '..', 'SchoolImages', newFileName), async (error) => {
-        //         if (error) {
-        //             return res.status(400).json({ message: error.message });
-        //         } else {
-        //             const newSchool = new School({
-        //                 name,
-        //                 address,
-        //                 location,
-        //                 phoneNumber,
-        //                 description,
-        //                 successRate,
-        //                 reviews,
-        //                 likes,
-        //                 dislikes,
-        //                 trainings,
-        //                 thumbnail: newFileName,
-        //                 owner: userId
-        //             });
-        //             await newSchool.save();
-        //             if (!newSchool) {
-        //                 return res.status(400).json({ message: 'Error creating school' });
-        //             }
-        //             res.status(200).json({ message: 'School created successfully!', newSchool });
-        //         }    
-        //     });
-        // } catch (error) {
-        //     res.status(400).json({ message: error.message });
-        // }
             
     } else {
         res.status(403).json({ message: 'You are not logged in!' });
     }
 });
 
-// get all schools
 
+// get all schools
 router.get("/", async (req, res) => {
     try {
         const schools = await School.find();
@@ -115,37 +72,65 @@ router.get("/:id", async (req, res) => {
 });
 
 // update a school by ID
-router.put("/:id", school.array('images', 5),  async (req, res) => {
+router.put("/:id",  async (req, res) => {
     if (req.session.user) {
         try {
             const school = await School.findById(req.params.id);
-            if (school.owner == req.session.user._id) {
-                if (req.body.name && req.body.address && req.body.location && req.body.phoneNumber && req.body.description) {
-                    newSchoolData = JSON.parse(JSON.stringify(req.body));
-                    if (req.files.length > 0) {
-                        const fileUrls = req.files.map(file => ({
-                            filename: file.filename,
-                            url: `${req.protocol}://${req.get('host')}/SchoolImages/${userId}/${file.filename}`
-                        }));
-                        newSchoolData.images = fileUrls;
-                        const oldImages = school.images;
-                        if (oldImages && oldImages.length > 0) {
-                            oldImages.forEach(image => {
-                                fs.unlinkSync(path.join(__dirname, '..', 'SchoolImages', userId, image.filename));
+            if (school.owner == req.session.user._id || req.session.user.isAdmin) {
+                const updatedSchool = {};
+                // check if the user wants to update the image
+                if (req.files) {
+                    if (req.files.thumbnail) {
+                        // delete the old image
+                        if (school.thumbnail) {
+                            fs.unlink(path.join(__dirname, '../uploads', school.thumbnail) , (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
                             });
                         }
+                        const file = req.files.thumbnail;
+                        if (file.size > 2000000) {
+                            return res.status(400).json({ message: 'Image size should not exceed 2MB' });
+                        }
+                        let fileName = file.name;
+                        let splitedName = fileName.split('.');
+                        let newFileName = splitedName[0] + uuid() + '.' + splitedName[splitedName.length - 1];
+                        file.mv(path.join(__dirname, '../uploads', newFileName), async (err) => {
+                            if (err) {
+                                return res.status(500).json({ message: 'The file was not uploaded!' });
+                            }
+                        });
+                        updatedSchool.thumbnail = newFileName;
                     }
-                    await School.findByIdAndUpdate(req.params.id, newSchoolData, {new: true});
-                    res.status(200).json({ message: 'School updated successfully!' });
                 }
+                if (req.body) {
+                    try {
+                        const { name, address, location, phoneNumber, description, successRate, reviews, likes, dislikes, trainings } = req.body;
+                        updatedSchool.name = name || school.name;
+                        updatedSchool.address = address || school.address;
+                        updatedSchool.location = location || school.location;
+                        updatedSchool.phoneNumber = phoneNumber || school.phoneNumber;
+                        updatedSchool.description = description || school.description;
+                        updatedSchool.successRate = successRate || school.successRate;
+                        updatedSchool.reviews = reviews || school.reviews;
+                        updatedSchool.likes = likes || school.likes;
+                        updatedSchool.dislikes = dislikes || school.dislikes;
+                        updatedSchool.trainings = trainings || school.trainings;
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+                await School.findByIdAndUpdate(req.params.id, updatedSchool, { new: true });
+                res.status(200).json({ message: 'School updated successfully!' });
+                
             } else {
                 res.status(403).json({ message: 'You are not authorized to update this school!' });
             }
         } catch (error) {
             res.status(400).json({ message: error.message });
         }
-    } else {
-        res.status(403).json({ message: 'You are not logged in Yezt !!!' });
+    
     }
 });
 
