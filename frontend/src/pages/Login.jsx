92bc5113b977a0c83017e248dde7299a331e9a8f
st sama <albertezoula98@gmail.com>
2024-07-17 11:29:47 +0000

    adding the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [userData, setUserData] = useState({ email: '', password: ''});
+
+  const chanceInputHandler = (e) => {
+    setUserData(prevState => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+
   return (
-    <div>Login</div>
+    <section className='login'>
+      <div className="container">
+        <h2>Login</h2>
+        <form className="form login__form">
+          <p className="form__error-message">This is an error message</p>
+          <input type="email" placeholder='Enter your email'name='email' value={userData.email} onChange={chanceInputHandler} />
+          <input type="password" placeholder='Enter your password'name='password' value={userData.password} onChange={chanceInputHandler} />
+          <button className='btn primary'>Login</button>
+        </form>
+        <small>Don't have an account? <Link to='/register'>Sign Up</Link> </small>
+      </div>
+    </section>
   )
 }
 
