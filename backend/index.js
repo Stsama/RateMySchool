bf86c3d04abb395cf4261a839e2c41308b38d623
st sama <albertezoula98@gmail.com>
2024-07-24 00:37:45 +0000

    adding the patch method to cors allowed methods

--- backend/index.js
+++ backend/index.js
@@ -81,7 +81,7 @@ app.use((req, res, next) => {
 });
 app.use(cors({
   origin:  `${process.env.FRONTEND_URL}`,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', "PATCH" ,'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Permettre l'envoi de cookies
 }));
